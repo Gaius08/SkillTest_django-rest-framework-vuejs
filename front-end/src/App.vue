07fe98b0b6059237aa41056c99ac3f72e2f650d2
Gaius08 <gaiusalfred@gmail.com>
2023-04-27 16:43:21 +0530

    implemented the router transistion

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,6 +1,8 @@
 <template>
-<div>
-    <router-view />
+  <div>
+    <transition name="fade" mode="out-in">
+      <router-view />
+    </transition>
   </div>
 </template>
 <script>
@@ -17,3 +19,16 @@ export default {
   },
 };
 </script>
+<style>
+.fade-enter-active {
+  transition: all 0.3s ease
+}
+.fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.fade-enter-from,
+.fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
+</style>
